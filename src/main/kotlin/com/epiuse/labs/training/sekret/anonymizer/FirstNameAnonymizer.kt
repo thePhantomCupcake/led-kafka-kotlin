cdc17434bcd8f77584199c166ea66a1f24bb5f5f
JoDan Alberts <jodan@labs.epiuse.com>
2020-04-16 23:43:58 +0200

    Make FirstNameAnonymizer deterministic

--- src/main/kotlin/com/epiuse/labs/training/sekret/anonymizer/FirstNameAnonymizer.kt
+++ src/main/kotlin/com/epiuse/labs/training/sekret/anonymizer/FirstNameAnonymizer.kt
@@ -1,12 +1,24 @@
 package com.epiuse.labs.training.sekret.anonymizer
 
+import com.github.javafaker.*
 import org.springframework.stereotype.Component
+import java.util.concurrent.ConcurrentHashMap
 
 @Component
-class FirstNameAnonymizer: Anonymizer<String, String> {
+class FirstNameAnonymizer : Anonymizer<String, String> {
+
+    private val faker = Faker()
+
+    private val firstNames = ConcurrentHashMap<String, String>()
 
     override fun anonymize(input: String): String {
-        return "John"
+        return if (firstNames.containsKey(input)) {
+            firstNames[input]!!
+        } else {
+            val newName = faker.name().firstName()
+            firstNames[input] = newName
+            newName!!
+        }
     }
 
 }
---
+++ src/main/kotlin/com/epiuse/labs/training/sekret/binding/MyProcessor.kt
@@ -0,0 +1,21 @@
+package com.epiuse.labs.training.sekret.binding
+
+import org.springframework.cloud.stream.annotation.Input
+import org.springframework.cloud.stream.annotation.Output
+import org.springframework.messaging.MessageChannel
+import org.springframework.messaging.SubscribableChannel
+
+
+interface MyProcessor {
+
+    @Input(INPUT)
+    fun input(): SubscribableChannel
+
+    @Output(OUTPUT)
+    fun output(): MessageChannel?
+
+    companion object {
+        const val INPUT = "my-producer-in"
+        const val OUTPUT = "my-producer-out"
+    }
+}
