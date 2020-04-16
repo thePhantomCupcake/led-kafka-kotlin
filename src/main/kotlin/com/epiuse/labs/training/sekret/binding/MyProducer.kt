e93d1ab9aa0758668246b4072ef5cd7535f5de67
JoDan Alberts <jodan@labs.epiuse.com>
2020-04-16 22:38:57 +0200

    Add task to implement processor

---
+++ src/main/kotlin/com/epiuse/labs/training/sekret/binding/MyProducer.kt
@@ -0,0 +1,15 @@
+package com.epiuse.labs.training.sekret.binding
+
+import org.springframework.cloud.stream.annotation.Output
+import org.springframework.messaging.MessageChannel
+
+
+interface MyProducer {
+
+    @Output(OUTPUT)
+    fun output(): MessageChannel?
+
+    companion object {
+        const val OUTPUT = "my-producer-out"
+    }
+}
