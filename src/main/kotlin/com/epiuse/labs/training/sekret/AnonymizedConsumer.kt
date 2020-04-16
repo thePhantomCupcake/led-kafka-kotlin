c3865ae5f48199680b70f77c7d766875f23d09a2
JoDan Alberts <jodan@labs.epiuse.com>
2020-04-16 23:34:51 +0200

    Add faker to producer and reimplement consumer

--- src/main/kotlin/com/epiuse/labs/training/sekret/AnonymizedConsumer.kt
+++ src/main/kotlin/com/epiuse/labs/training/sekret/AnonymizedConsumer.kt
@@ -1,15 +1,18 @@
 package com.epiuse.labs.training.sekret
 
 import com.epiuse.labs.training.sekret.model.User
-import org.apache.kafka.streams.kstream.KStream
 import org.slf4j.LoggerFactory
 import org.springframework.cloud.stream.annotation.EnableBinding
 import org.springframework.cloud.stream.annotation.StreamListener
 import org.springframework.cloud.stream.messaging.Sink
 
+@EnableBinding(Sink::class)
 class AnonymizedConsumer {
 
-    //TODO: implement consumer using default Sink binding to read from "anonymous" topic
+    @StreamListener(Sink.INPUT)
+    fun consume(value: User) {
+        LOGGER.info("Successfully anonymized user with result: $value")
+    }
 
     companion object {
         val LOGGER = LoggerFactory.getLogger(AnonymizedConsumer::class.java)
