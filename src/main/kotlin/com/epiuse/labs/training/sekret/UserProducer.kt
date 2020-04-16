04db94bba2b4c694afc74002a3d77d6ac87df8ab
JoDan Alberts <jodan@labs.epiuse.com>
2020-04-16 23:28:33 +0200

    Add faker to producer

--- src/main/kotlin/com/epiuse/labs/training/sekret/UserProducer.kt
+++ src/main/kotlin/com/epiuse/labs/training/sekret/UserProducer.kt
@@ -1,6 +1,8 @@
 package com.epiuse.labs.training.sekret
 
+import com.epiuse.labs.training.sekret.binding.MyProducer
 import com.epiuse.labs.training.sekret.model.User
+import com.github.javafaker.Faker
 import org.springframework.cloud.stream.annotation.EnableBinding
 import org.springframework.integration.annotation.InboundChannelAdapter
 import org.springframework.integration.annotation.Poller
@@ -9,15 +11,18 @@ import org.springframework.messaging.MessageHeaders.CONTENT_TYPE
 import org.springframework.messaging.support.MessageBuilder
 import org.springframework.util.MimeTypeUtils.APPLICATION_JSON
 
-import com.github.javafaker.*
-import com.epiuse.labs.training.sekret.binding.MyProducer
-
 @EnableBinding(MyProducer::class)
 class UserProducer {
 
+    private val faker = Faker()
+
     @InboundChannelAdapter(channel = MyProducer.OUTPUT, poller = [Poller(fixedRate = "1000")])
     fun generate(): Message<*>? {
-        val value = User(++x, "James", "Brown", "jbrown@labs.co.za")
+        val firstName = faker.name().firstName()
+        val lastName = faker.name().lastName()
+        val emailAddress = "${firstName[0]}${lastName}@labs.com".toLowerCase()
+
+        val value = User(++x, firstName, lastName, emailAddress)
         println("Sending: $value")
         return MessageBuilder.withPayload(value)
                 .setHeader(CONTENT_TYPE, APPLICATION_JSON)
@@ -26,7 +31,6 @@ class UserProducer {
     }
 
     companion object {
-
         var x = 0L
     }
 }
